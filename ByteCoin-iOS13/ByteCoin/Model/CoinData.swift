f28d6f4192b3d822cf01bf64325e5fee7c831973
jareyesda <59104004+jareyesda@users.noreply.github.com>
2021-05-23 20:09:52 -0400

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Juan Reyes on 4/7/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData {
+    let 
+}
